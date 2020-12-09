bd755cb2855d2867fb3df2c1b5f40ccb31641ed7
madhuvayya <madhu.vayya@gmail.com>
2020-12-09 21:15:01 +0530

    Added code to display employee details using template literals

---
+++ js/home.js
@@ -0,0 +1,35 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    createInnerHtml();
+});
+
+const createInnerHtml = () => {
+    const innerHtml = `
+    <tr>
+    <th></th>
+    <th>Name</th>
+    <th>Gender</th>
+    <th>Department</th>
+    <th>Salary</th>
+    <th>Start Date</th>
+    <th>Actions</th>
+    </tr> 
+        <tr>
+            <td>
+                <img class="profile" alt="" src="../assets/profile-images/Ellipse -2.png">
+            </td>
+            <td>Narayan Mahadevan</td>
+            <td>Male</td>
+            <td>
+                <div class="dept-label">HR</div>
+                <div class="dept-label">Finance</div>
+            </td>
+            <td>300000</td>
+            <td>1 NOV 2020</td>
+            <td>
+                <img id="1" title="Delete" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
+                <img id="1" title="Update" onclick="update(this)" alt="edit" src="../assets/icons/create-black-18dp.svg">                    
+            </td>
+        </tr>
+    `;
+    document.querySelector('#display').innerHTML = innerHtml;
+}
