4b295c38e3f577af98021054757fa6f8451476b2
madhuvayya <madhu.vayya@gmail.com>
2020-12-09 21:21:59 +0530

    Refactored code to separate table header from table body content

--- js/home.js
+++ js/home.js
@@ -3,16 +3,10 @@ window.addEventListener('DOMContentLoaded', (event) => {
 });
 
 const createInnerHtml = () => {
-    const innerHtml = `
-    <tr>
-    <th></th>
-    <th>Name</th>
-    <th>Gender</th>
-    <th>Department</th>
-    <th>Salary</th>
-    <th>Start Date</th>
-    <th>Actions</th>
-    </tr> 
+    const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>"+
+                        "<th>Salary</th><th>Start Date</th><th>Actions</th>";
+
+    const innerHtml = `${headerHtml} 
         <tr>
             <td>
                 <img class="profile" alt="" src="../assets/profile-images/Ellipse -2.png">
