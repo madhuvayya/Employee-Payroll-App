e0ce9209b8cb3f6ba89f5ac21a9f711121fbdc59
madhuvayya <madhu.vayya@gmail.com>
2020-12-12 21:19:18 +0530

    Added code to reset the employee payroll form

--- js/EmployeePayroll.js
+++ js/EmployeePayroll.js
@@ -173,3 +173,36 @@ const createAndUpdateStorage = (empPayrollData) => {
     // alert(empPayrollList.toString());
     localStorage.setItem("EmployeePayrollList",JSON.stringify(empPayrollList));
 }
+
+const resetForm = () => {
+    setValue('#name','');
+    unsetSelectedValues('[name=profile]');
+    unsetSelectedValues('[name=gender]');
+    unsetSelectedValues('[name=department]');
+    setValue('#salary','');
+    setValue('#notes','');
+    setValue('#day','1');
+    setValue('#month','January');
+    setValue('#year',new Date().getFullYear());
+    document.querySelector('.salary-output').textContent = '400000';
+}
+
+const unsetSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    allItems.forEach( item => {
+        item.checked = false;
+    });    
+}
+
+const setTextValue = (id, value) => {
+    const element = document.querySelector(id);
+    element.textContent = value;
+}
+
+const setValue = (id, value) => {
+    const element = document.querySelector(id);
+    element.value = value;
+}
+
+
+
